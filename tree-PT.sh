79860d652329bfe8e918b1e8075be42c869d266b
Ravi <103545177+ravivalentim@users.noreply.github.com>
2025-12-11 20:57:03 -0300

    portuguese version is now working

--- tree-PT.sh
+++ tree-PT.sh
@@ -32,8 +32,8 @@ for ((i=1; i<=2; i++))
 new_year=$(date +'%Y')
 let new_year++
 tput setaf 1; tput bold
-tput cup $lin $((c - 6)); echo FELIZ NATAL
-tput cup $((lin + 1)) $((c - 10)); echo E muito CÓDIGO em $new_year
+tput cup $lin $((c - 6)); echo BOAS FESTAS
+tput cup $((lin + 1)) $((c - 9)); echo E muito CÓDIGO em $new_year
 let c++
 k=1
 
@@ -60,7 +60,7 @@ while true; do
         sh=1
         for l in C Ó D I G O
         do
-            tput cup $((lin+1)) $((c+sh))
+            tput cup $((lin+1)) $((c-3+sh))
             echo $l
             let sh++
             sleep 0.01
@@ -68,3 +68,4 @@ while true; do
     }
     k=$((k % 2 + 1))
 done
+
