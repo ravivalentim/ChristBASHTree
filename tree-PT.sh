af833ce5e3f47c9e90c2640dbcc2325c5aa510b6
Ravi <103545177+ravivalentim@users.noreply.github.com>
2025-12-11 20:41:37 -0300

    new commit

--- tree-PT.sh
+++ tree-PT.sh
@@ -33,7 +33,7 @@ new_year=$(date +'%Y')
 let new_year++
 tput setaf 1; tput bold
 tput cup $lin $((c - 6)); echo FELIZ NATAL
-tput cup $((lin + 1)) $((c - 10)); echo E muito código em $new_year
+tput cup $((lin + 1)) $((c - 10)); echo E muito CODE em $new_year
 let c++
 k=1
 
@@ -58,7 +58,7 @@ while true; do
         color=$(((color+1)%8))
         # Flashing text
         sh=1
-        for l in C O D E
+        for l in C Ó D I G O
         do
             tput cup $((lin+1)) $((c+sh))
             echo $l
